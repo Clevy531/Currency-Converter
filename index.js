29d69294d6cb263b81b13bb3c4927a9694477faa
Clevy531 <clevy531@bu.edu>
2026-01-30 23:39:36 -0500

    Add Currency Converter project files

---
+++ index.js
@@ -0,0 +1,28 @@
+import { API_KEY } from './api.js';
+
+const currencyFirstEl = document.getElementById("currency-first");
+
+const worthFirstEl = document.getElementById("worth-first");
+
+const currencySecondEl = document.getElementById("currency-second");
+
+const worthSecondEl = document.getElementById("worth-second");
+
+const exchangeRateEl = document.getElementById("exchange-rate");
+
+updateRate();
+
+function updateRate(){
+    fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${currencyFirstEl.value}`).then((res)=>res.json()).then((data)=>{
+        const rate = data.conversion_rates[currencySecondEl.value];
+        exchangeRateEl.innerText = `1 ${currencyFirstEl.value} = ${rate + " " + currencySecondEl.value}`;
+        worthSecondEl.value = (worthFirstEl.value * rate).toFixed(2);
+    });
+}
+
+currencyFirstEl.addEventListener("change", updateRate);
+
+currencySecondEl.addEventListener("change", updateRate);
+
+worthFirstEl.addEventListener("input", updateRate)
+
